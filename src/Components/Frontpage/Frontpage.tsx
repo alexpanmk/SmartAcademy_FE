f19bd8b3854775a41c6fbe39202988e163089754
alexpanmk <alexpanmk@gmail.com>
2024-02-23 14:17:10 +0800

    Set up Navbar, Frontpage components

--- src/Components/Frontpage/Frontpage.tsx
+++ src/Components/Frontpage/Frontpage.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// UI Components
+import { AppShell } from '@mantine/core'
+import { NavbarMinimal } from '../NavbarMinimal/NavbarMinimal'
 
 function Frontpage() {
     return (
-        <div>Frontpage</div>
+        <AppShell
+            padding="md"
+            header={{ height: 60 }}
+        // navbar={{
+        //     width: 300,
+        //     breakpoint: 'sm',
+        //     collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        // }}
+        >
+            <AppShell.Header>Header</AppShell.Header>
+            <AppShell.Navbar>
+                <NavbarMinimal />
+            </AppShell.Navbar>
+            <AppShell.Main>
+                {/* <Button onClick={toggleDesktop} visibleFrom="sm">
+                    Toggle navbar
+                </Button>
+                <Button onClick={toggleMobile} hiddenFrom="sm">
+                    Toggle navbar
+                </Button> */}
+            </AppShell.Main>
+        </AppShell>
     )
 }
 
