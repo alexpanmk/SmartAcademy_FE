baf4b909cf81776a2155cb7cc8f0afdd82f06b86
alexpanmk <alexpanmk@gmail.com>
2024-02-23 17:45:50 +0800

    add: stub CourseListPage component

---
+++ src/Components/CourseListPage/CourseListPage.tsx
@@ -0,0 +1,11 @@
+//Page to show list of course
+//To show different views for learner and course creators
+import React from 'react'
+
+function CourseListPage() {
+    return (
+        <div>CourseListPage</div>
+    )
+}
+
+export default CourseListPage
