30d9ecae3ec5ac47040ac2c0a3c188d84bb944ba
alexpanmk <alexpanmk@gmail.com>
2024-02-23 12:11:53 +0800

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
+import { React, useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+// TODO: import { getUser } from "./service/users";
 
 import { AppShell } from '@mantine/core'
 
 //UI Components
 import { AuthenticationImage } from './Components/AuthenticationImage/AuthenticationImage'
+import Frontpage from "./Components/Frontpage/Frontpage";
+
+//SA Components
 
 function App() {
-
+  const [user, setUser] = useState("alexpan@gmail.com")
 
   return (
-    <>
-      <AuthenticationImage />
-    </>
+    <Router>
+      {user ? (
+        <Routes>
+          <Route path="/" element={<Frontpage />} />
+        </Routes>
+      )
+        : (
+          <Routes>
+            <Route path="/" element={<AuthenticationImage />} />
+            <Route path="/signup" element={<AuthenticationImage />} />
+          </Routes>
+        )}
+    </Router >
   )
 }
-
 export default App
