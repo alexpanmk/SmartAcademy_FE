a8281dd8f6171ab83a869e53a676ca935493aa00
alexpanmk <alexpanmk@gmail.com>
2024-02-23 17:50:30 +0800

    add: stub CourseViewPage

---
+++ src/Components/CourseViewPage/CourseViewPage.tsx
@@ -0,0 +1,11 @@
+//Component for course view. Creator's permission will allow creator option to edit course
+//To show stats for creator and learner(completed or not/progress/score etc.)
+import React from 'react'
+
+function CourseViewPage() {
+    return (
+        <div>CourseViewPage</div>
+    )
+}
+
+export default CourseViewPage
