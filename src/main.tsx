44d4d01a1951e8f860c44d8479debf65ff6190ac
alexpanmk <alexpanmk@gmail.com>
2024-02-24 11:51:36 +0800

    wrapped clerk auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,25 @@ import '@mantine/core/styles/global.css';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+import { ClerkProvider } from '@clerk/clerk-react'
 import { MantineProvider } from '@mantine/core';
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </ClerkProvider>
   </React.StrictMode>,
 )
