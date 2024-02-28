3bfc885087c0fdae040d1c0a674d9a296edf1911
alexpanmk <alexpanmk@gmail.com>
2024-02-28 09:37:36 +0800

    add: question cards for learnerview

--- src/Components/LearnerView/LearnerView.tsx
+++ src/Components/LearnerView/LearnerView.tsx
@@ -2,14 +2,50 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
+import { Space, Stack, Flex, Paper, Title, Text, Group, Button } from '@mantine/core'
+
+import useCourseStore from '../../stores/useCourseStore'
 
 function LearnerView() {
 
+    const { courses } = useCourseStore()
     const { courseId } = useParams()
+    const [course, setCourse] = useState(
+        courses.find((course) => course._id === courseId)
+    )
 
+    const questions = course.questions
 
     return (
-        <div>LearnerView: {courseId}</div>
+        <>
+            <Group justify='flex-start'>
+                <Title order={1}>{course.title}</Title>
+            </Group>
+
+            <Space h="xl" />
+
+            <Stack direction="column" spacing="md">
+                {questions.map((question: any, index: number) => {
+                    return (
+                        <>
+                            <Paper miw={1000} shadow="xs" p="xl" direction="column" justify="space-between">
+                                <Title order={3}>Question: {question.questionText}</Title>
+                                <Space h="xl" />
+                                <Group direction="column" spacing="md">
+                                    {question.options.map((option: any, index: number) => {
+                                        return (
+                                            <Button size="md" variant="light" radius="md">{option}</Button>
+                                        )
+                                    })}
+                                </Group>
+                            </Paper>
+                            <Space h="xl" />
+                        </>
+                    )
+                })}
+            </Stack>
+
+        </>
     )
 
 }
