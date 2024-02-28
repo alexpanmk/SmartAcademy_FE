1ad3afe2b297197627d91505230d8b3eddcfb980
alexpanmk <alexpanmk@gmail.com>
2024-02-28 15:58:02 +0800

    add handleFieldsChange

--- src/Components/CourseEditView/CourseEditView.tsx
+++ src/Components/CourseEditView/CourseEditView.tsx
@@ -39,7 +39,7 @@ function CourseEditView(props) {
     //DONE: To load course according to the id
     //TODO: Save course when save button is clicked
     //TODO: when courseID is falsy means course is new.
-
+    //TODO: Cancel to confirm discard changes
 
 
     useEffect(() => {
@@ -63,39 +63,54 @@ function CourseEditView(props) {
     const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current))
     // Edit or Create new for cse
 
+    const handleSave = () => {
+
+    }
+
+    const handleFieldChange = (e) => {
+        const { id, value } = e.target;
+        setCourse({ ...course, [id]: value })
+    }
+
     return (
         <>
             <Title order={1}>{isEditMode ? "Edit Course" : "NewCourse"}</Title>
 
             <Space h="xl" />
 
-            <Stepper active={active} onStepClick={setActive}>
-                <Stepper.Step label="Course Details">
-                    <Stack direction="column" spacing="md">
+            <Stack direction="column" justify='space-between'>
+                <Stepper active={active} onStepClick={setActive}>
+                    <Stepper.Step label="Course Details">
+                        <Stack direction="column" spacing="md">
 
-                        {/* <input type="text" placeholder="Course Image URL" /> */}
-                        <input value={course.title} type="text" placeholder="Course Title" />
-                        <textarea value={course.description} placeholder="Course Description" />
+                            {/* <input type="text" placeholder="Course Image URL" /> */}
+                            <Text fw="bold">Course Title</Text>
+                            <input onChange={handleFieldChange} id="title" value={course.title} type="text" placeholder="Course Title" />
+                            <Text fw="bold">Course Description</Text>
+                            <textarea id="description" value={course.description} placeholder="Course Description" />
+                            <Space h="xl" />
+                        </Stack>
+                        <Group justify={"flex-end"}>
+                            <Button onClick={nextStep}>Next</Button>
+                            <Button onClick={handleSave}>Save</Button>
+                            <Button onClick={closeEditModal}>Cancel</Button>
+                        </Group>
+                    </Stepper.Step>
+                    <Stepper.Step label="Course Content">
+                        <ContentBuilder />
                         <Space h="xl" />
-                    </Stack>
-                    <Group justify={"flex-end"}>
-                        <Button onClick={nextStep}>Next</Button>
-                        <Button>Save</Button>
-                        <Button onClick={closeEditModal}>Cancel</Button>
-                    </Group>
-                </Stepper.Step>
-                <Stepper.Step label="Course Content">
-                    <ContentBuilder />
-                    <Space h="xl" />
-                    <Group justify={"flex-end"}>
-                        <Button onClick={prevStep}>Previous</Button>
-                        <Button onClick={nextStep}>Next</Button>
-                        <Button>Save</Button>
-                        <Button onClick={closeEditModal}>Cancel</Button>
-                    </Group>
-                </Stepper.Step>
-                <Stepper.Step label="Course Settings" />
-            </Stepper >
+                        <Group justify={"flex-end"}>
+                            <Button onClick={prevStep}>Previous</Button>
+                            <Button onClick={nextStep}>Next</Button>
+                            <Button>Save</Button>
+                            <Button onClick={closeEditModal}>Cancel</Button>
+                        </Group>
+                    </Stepper.Step>
+                    <Stepper.Step label="Course Settings" />
+                </Stepper >
+
+
+            </Stack>
 
 
         </>
